7e40b678b88c0e148140db189ded14c426aa490c
UnteRT <vfmemo23@gmail.com>
2023-10-05 12:43:25 +0300

    commit1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,9 +1,22 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <math.h>
 using namespace std;
 int main()
 {
 	HWND hWnd = GetConsoleWindow();
-	HDC hDC
-
+	HDC hDC = GetDC(hWnd);
+	HPEN Pen = CreatePen(PS_SOLID, 2, RGB(255, 255, 255));
+	SelectObject(hDC, Pen);
+	MoveToEx(hDC, 0, 85, NULL);
+	LineTo(hDC, 200, 85);
+	MoveToEx(hDC, 100, 0, NULL);
+	LineTo(hDC, 100, 170);
+	for (float x = -8.0f; x <= 8.0f; x += 0.1)
+	{
+		MoveToEx(hDC, 10 * x + 100, -10 * sin(x) + 85, NULL);
+		LineTo(hDC, 10 * x + 100, -10 * sin(x) + 85);
+	}
+	ReleaseDC(hWnd, hDC);
+	cin.get();
 }
