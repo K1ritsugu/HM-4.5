c2f4072545e47cdab73bb912510a956837b476b0
UnteRT <vfmemo23@gmail.com>
2023-10-26 02:01:58 +0300

    1.0

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,46 +1,33 @@
 ﻿#include <iostream>
 #include <string>
-int m, c, coun, start, str1, h;
+int m, i, c, coun, start, str1, h;
+char str;
 using namespace std;
-int swap(char s) {
-	m = int(s) - 48;
-	if (m < 10) {
-		return m;
+int generator(int start, int str1) {
+	switch (str1) {
+	case 1:
+		m = 37, i = 3, c = 64;
+		return (m * start + i) % c;
+	case 2:
+		m = 25173, i = 13849, c = 65537;
+		return (m * start + i) % c;
 	}
-	else {
-		return m - 7;
-	}
-
 }
 int main()
 {
-	string str;
 	setlocale(0, "");
-	cout << "Введите число в начальном основании" << endl;
-	cin >> str;
-	cout << "Введите начальное основание" << endl;
-	cin >> h;
-	cout << "Введите итоговое основание" << endl;
-	cin >> c;
-	if (h < 2 || h > 16 || c < 2 || c > 16) {
-		cout << "Ошибка";
-		return 0;
-	}
-	int itog_10 = 0;
-	int j = 0;
-
-	for (int i = str.length() - 1; i > -1; i--) {
-		if (swap(str[i]) < 0 || swap(str[i]) > 15) {
-			cout << "Ошибка";
+	cout << "Выберите начальное число" << endl;
+	cin >> start;
+	while (true) {
+		cout << "Сгенерировать следующее число? Y/N" << endl;
+		cin >> str;
+		if (str == 'N') {
 			return 0;
 		}
-		itog_10 += swap(str[i]) * pow(h, j);
-		j += 1;
+		cout << "Выберите вариант генерации 1/2" << endl;
+		cin >> str1;
+		h = generator(start, str1);
+		cout << h << endl;
+		start = h;
 	}
-	string itog = "";
-	while (itog_10 > 0) {
-		itog = to_string(itog_10 % c) + itog;
-		itog_10 = itog_10 / c;
-	}
-	cout << itog;
 }
